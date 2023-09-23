96288b881b419ca32db1657fb338c2ae43d60858
Ehtical-Hacker-Krishanu01 <krishanukarmakar.kk94@gmail.com>
2023-09-23 13:35:45 +0530

    Template Classes of Real and Imaginary Same

--- ComplexCalculator/ComplexCalculator.cpp
+++ ComplexCalculator/ComplexCalculator.cpp
@@ -6,21 +6,20 @@
 
 using namespace std;
 
-template <class R, class I>
+template <class R>
 class ComplexCalculator{
 
     private:
-        R real=0;
-        I imaginary=0;
+        R real=0, imaginary=0;
 
     public:
         ComplexCalculator(){}
 
-        ComplexCalculator(R real, I imaginary=0) : real(real), imaginary(imaginary){}
+        ComplexCalculator(R real, R imaginary=0) : real(real), imaginary(imaginary){}
         R getReal(){
             return real;
         }
-        I getImaginary(){
+        R getImaginary(){
             return imaginary;
         }
 
@@ -73,10 +72,10 @@ class ComplexCalculator{
 
             else if (power > 1)
             {   
-                ComplexCalculator<R,I> TEMP(this->real,this->imaginary);
+                ComplexCalculator<R> TEMP(this->real,this->imaginary);
                 for (int i=2;i<=power;i++)
                 {
-                    ComplexCalculator<R,I> temp =  (TEMP)*(*this);
+                    ComplexCalculator<R> temp =  (TEMP)*(*this);
                     TEMP.real = temp.real;
                     TEMP.imaginary = temp.imaginary;
                 }
@@ -96,66 +95,72 @@ class ComplexCalculator{
         }
 
 
-        ComplexCalculator<double,double> operator/(const ComplexCalculator& other){
-            double a,b,c,d;
-            a = double(real);
-            b = double(imaginary);
-            c = double(other.real);
-            d = double(other.imaginary);
-            return ComplexCalculator<double,double>(((a*c+b*d)/(c*c+d*d)),((b*c-a*d)/(c*c+d*d)));
+        ComplexCalculator<R> operator/(const ComplexCalculator& other){
+            R a,b,c,d;
+            a = R(real);
+            b = R(imaginary);
+            c = R(other.real);
+            d = R(other.imaginary);
+            return ComplexCalculator<R>(((a*c+b*d)/(c*c+d*d)),((b*c-a*d)/(c*c+d*d)));
         }
 
 
-        static ComplexCalculator<double,double> ComplexSin(ComplexCalculator &obj){
-            double a = double(obj.real);
-            double b = double (obj.imaginary);
-            return ComplexCalculator<double,double>(sin(a)*cosh(b)+cos(a)*sinh(b),0);
+        static ComplexCalculator<R> ComplexSin(ComplexCalculator &obj){
+            R a = R(obj.real);
+            R b = R (obj.imaginary);
+            return ComplexCalculator<R>(sin(a)*cosh(b)+cos(a)*sinh(b),0);
         }
 
 
-        static ComplexCalculator<double,double> ComplexCos(ComplexCalculator &obj){
-            double a = double(obj.real);
-            double b = double (obj.imaginary);
-            return ComplexCalculator<double,double>(cos(a)*cosh(b)-sin(a)*sinh(b),0);            
+        static ComplexCalculator<R> ComplexCos(ComplexCalculator &obj){
+            R a = R(obj.real);
+            R b = R (obj.imaginary);
+            return ComplexCalculator<R>(cos(a)*cosh(b)-sin(a)*sinh(b),0);            
         }
 
 
-        static ComplexCalculator<double,double> ComplexTan( ComplexCalculator &obj){
-            double a = double(obj.real);
-            double b = double (obj.imaginary);            
-            return ComplexCalculator<double,double>((tan(a)+tanh(b))/(1-tan(a)*tanh(b)),0); 
+        static ComplexCalculator<R> ComplexTan( ComplexCalculator &obj){
+            R a = R(obj.real);
+            R b = R (obj.imaginary);            
+            return ComplexCalculator<R>((tan(a)+tanh(b))/(1-tan(a)*tanh(b)),0); 
         }
 
 
-        static ComplexCalculator<double, double> ComplexCot( ComplexCalculator obj) {
-            return ComplexCalculator<double, double>(1) / ComplexTan(obj);
+        static ComplexCalculator<R> ComplexCot( ComplexCalculator obj) {
+            return ComplexCalculator<R>(1) / ComplexTan(obj);
         }
 
 
-        static ComplexCalculator<double,double> ComplexSec(ComplexCalculator &obj){
-            return ComplexCalculator<double, double>(1) / ComplexCos(obj);
+        static ComplexCalculator<R> ComplexSec(ComplexCalculator &obj){
+            return ComplexCalculator<R>(1) / ComplexCos(obj);
         }
 
 
-        static ComplexCalculator<double,double> ComplexCsc(ComplexCalculator &obj){
-            return ComplexCalculator<double, double>(1) / ComplexSin(obj);
+        static ComplexCalculator<R> ComplexCsc(ComplexCalculator &obj){
+            return ComplexCalculator<R>(1) / ComplexSin(obj);
         }
 
 
-        static ComplexCalculator<double,double> ComplexLn(ComplexCalculator &obj){
-            double a,b,r,Q;
-            a = double(obj.real);
-            b = double(obj.imaginary);
+        static ComplexCalculator<R> ComplexLn(ComplexCalculator &obj){
+            R a,b,r,Q;
+            a = R(obj.real);
+            b = R(obj.imaginary);
             r = sqrt(a*a + b*b);
             Q = atan(b/a);
-            return ComplexCalculator<double,double>(log(r),Q);
+            return ComplexCalculator<R>(log(r),Q);
         }
 
         
-        static ComplexCalculator<double,double> ComplexLog(ComplexCalculator obj, double base){
+        static ComplexCalculator<R> ComplexLog(ComplexCalculator obj, R base){
             return ComplexLn(obj)/ComplexCalculator(log(base));
         }
 };
 int main(){
+
+    ComplexCalculator<int> num1(3,4);
+    ComplexCalculator<int> num2(5,6);
+
+    (num1/num2).displayComplex();
+
     return 0;
 }
